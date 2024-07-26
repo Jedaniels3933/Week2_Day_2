88d47d8077f8ccdfbf265b0fdb4310bb503b1617
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-07-26 18:41:06 -0400

    Update NEWHomework.py

--- NEWHomework.py
+++ NEWHomework.py
@@ -2,7 +2,6 @@
 python_reviews = [ "This product is really good. I'm impressed with its quality.", "The performance of this product is excellent. Highly recommended!", "I had a bad experience with this product. It didn't meet my expectations.", "This was a poor quality product. Wouldn't recommend it to anyone.", "The product was average. Nothing extraordinary about it." ]
 
 key_words = ["good", "excellent", "bad", "poor", "average"]
-
 for review in python_reviews:
     for word in key_words:
         if word in review:
